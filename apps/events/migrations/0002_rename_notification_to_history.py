f7c96325a8109501a44c7c74e1d15b04b6485a83
Elvis Mantilla <mantillaqed@gmail.com>
2026-07-07 17:07:33 -0400

    arqui

---
+++ apps/events/migrations/0002_rename_notification_to_history.py
@@ -0,0 +1,25 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameModel(
+            old_name='Notification',
+            new_name='History',
+        ),
+        migrations.AlterModelTable(
+            name='history',
+            table='historial',
+        ),
+        migrations.AlterField(
+            model_name='history',
+            name='id',
+            field=models.AutoField(db_column='id_historial', primary_key=True, serialize=False),
+        ),
+    ]
